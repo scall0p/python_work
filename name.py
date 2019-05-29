2c7dbce6443136aadafb0a7bac51376146a1b3e5
scallop <viniciusvieira59@outlook.com>
2019-05-29 10:58:19 -0300

    add name.py in project

---
+++ name.py
@@ -0,0 +1,8 @@
+first_name = "vinicius"
+last_name = "vieira"
+full_name = first_name + " " + last_name
+print("Meu nome é: " + first_name)
+print("Meu sobrenome é: " + last_name)
+print("Meu nome completo é: " + full_name + "!")
+message = "\nauthor: " + full_name.title()
+print(message)
